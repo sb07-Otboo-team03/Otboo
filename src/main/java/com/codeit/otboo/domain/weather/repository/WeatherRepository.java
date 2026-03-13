40fb8ed9832f177e19c4bfe50e023cec8a836785
yumi Kim <cdcd0214@gmail.com>
2026-03-13 17:23:15 +0900

    Refactor: Weather Repository 수정 - Long -> UUID

--- src/main/java/com/codeit/otboo/domain/weather/repository/WeatherRepository.java
+++ src/main/java/com/codeit/otboo/domain/weather/repository/WeatherRepository.java
@@ -3,5 +3,7 @@ package com.codeit.otboo.domain.weather.repository;
 import com.codeit.otboo.domain.weather.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherRepository extends JpaRepository<Weather,Long> {
+import java.util.UUID;
+
+public interface WeatherRepository extends JpaRepository<Weather, UUID> {
 }
