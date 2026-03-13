94f651203d20173cc2d7129c160016ab3595329c
seongman Joe <smjoe0302@naver.com>
2026-03-13 16:40:13 +0900

    feat: 알림 엔티티, dto 추가 및 인증 dto 추가

---
+++ src/main/java/com/codeit/otboo/domain/notification/dto/NotificationResponse.java
@@ -0,0 +1,16 @@
+package com.codeit.otboo.domain.notification.dto;
+
+import com.codeit.otboo.domain.notification.entity.Level;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record NotificationResponse(
+        UUID id,
+        LocalDateTime createAt,
+        UUID receiverId,
+        String title,
+        String content,
+        Level level
+) {
+}
