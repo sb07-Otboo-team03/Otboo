c8b9d1ce17bd26c46edfb8180a5a45d3b63eb5c3
yumi Kim <cdcd0214@gmail.com>
2026-03-13 16:53:51 +0900

    Feat: Weather Repository 추가

---
+++ src/main/java/com/codeit/otboo/domain/weather/repository/WeatherRepository.java
@@ -0,0 +1,7 @@
+package com.codeit.otboo.domain.weather.repository;
+
+import com.codeit.otboo.domain.weather.entity.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeatherRepository extends JpaRepository<Weather,Long> {
+}
