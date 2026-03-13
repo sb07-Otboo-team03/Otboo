013d957ae071789b12262935101d70f11cf6e14a
ChoiJiHye950 <principle950@daum.net>
2026-03-13 16:52:14 +0900

    fix: dto 필드명 수정
    - `directMessageId` 을 `id` 로 수정

--- src/main/java/com/codeit/otboo/domain/directmessage/dto/response/DirectMessageResponse.java
+++ src/main/java/com/codeit/otboo/domain/directmessage/dto/response/DirectMessageResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record DirectMessageResponse(
-        UUID directMessageId,
+        UUID id,
         LocalDateTime createdAt,
         UserResponse sender,
         UserResponse receiver,
