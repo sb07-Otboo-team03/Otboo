7640b6844bc31732483c687694b8cd638ca6d740
yumi Kim <cdcd0214@gmail.com>
2026-03-13 17:24:10 +0900

    Refactor: Weather 관련 DTO 수정 - *DTO -> *Response - class -> record

--- src/main/java/com/codeit/otboo/domain/weather/dto/response/WindSpeedResponse.java
+++ src/main/java/com/codeit/otboo/domain/weather/dto/response/WindSpeedResponse.java
@@ -2,7 +2,7 @@ package com.codeit.otboo.domain.weather.dto.response;
 
 import com.codeit.otboo.domain.weather.entity.WindAsWord;
 
-public class WindSpeedDto {
-    private Double speed;
-    private WindAsWord asWord;
-}
+public record WindSpeedResponse (
+    Double speed,
+    WindAsWord asWord
+){}
