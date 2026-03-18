c744cddd7dc1342a9231e85c7fe6cf02944c378d
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-03-18 18:50:56 +0900

    feat: User 엔티티 및 Profile 필드 수정
    변경 내용
    
    변경 이유

--- src/main/java/com/codeit/otboo/domain/user/dto/response/UserSummaryResponse.java
+++ src/main/java/com/codeit/otboo/domain/user/dto/response/UserSummaryResponse.java
@@ -1,18 +1,14 @@
 package com.codeit.otboo.domain.user.dto.response;
 
 import com.codeit.otboo.domain.user.entity.User;
+import lombok.Builder;
+
 import java.util.UUID;
 
+@Builder
 public record UserSummaryResponse(
    UUID userId,
    String name,
    String profileImageUrl
 ) {
-    public static UserSummaryResponse from(User user) {
-
-        String senderProfileImageUrl = "yml 명시 경로" + "/" + user.getProfile().getBinaryContent().getId().toString(); //??
-        UserSummaryResponse senderSummary = new UserSummaryResponse(user.getId(), user.getProfile().getName(), senderProfileImageUrl);
-
-        return new UserSummaryResponse(user.getId(), user.getProfile().getName(), senderProfileImageUrl);
-    }
 }
