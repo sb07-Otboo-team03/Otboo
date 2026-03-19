f85e931a8e773a51fe802ca4da4fad3cc0a1c9c1
yumi Kim <cdcd0214@gmail.com>
2026-03-19 13:17:12 +0900

    feat: ClothesAttributeDef의 Repository, Mapper 추가, ClothesAttributeValue의 Repository, Mapper 추가

---
+++ src/main/java/com/codeit/otboo/domain/clothes/attribute/attributedef/repository/ClothesAttributeDefRepository.java
@@ -0,0 +1,9 @@
+package com.codeit.otboo.domain.clothes.attribute.attributedef.repository;
+
+import com.codeit.otboo.domain.clothes.attribute.attributedef.entity.ClothesAttributeDef;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClothesAttributeDefRepository extends JpaRepository<ClothesAttributeDef, UUID> {
+}
