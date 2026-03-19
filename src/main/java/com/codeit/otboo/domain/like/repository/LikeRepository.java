66f6d1c996fcd32702beb30ee55735ca6e718719
gnara0719 <ppjw254236@gmail.com>
2026-03-19 14:15:45 +0900

    Feat: LikeService 및 LikeRepository 메서드 추가
    - feedLike 및 feedUnlike 서비스 로직 구현
    - LikeRepository에 existsByFeedIdAndUserId, findByFeedIdAndUserId 메서드 추가

--- src/main/java/com/codeit/otboo/domain/like/repository/LikeRepository.java
+++ src/main/java/com/codeit/otboo/domain/like/repository/LikeRepository.java
@@ -1,9 +1,14 @@
 package com.codeit.otboo.domain.like.repository;
 
 import com.codeit.otboo.domain.like.entity.Like;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, UUID> {
 
+    boolean existsByFeedIdAndUserId(UUID feedId, UUID userId);
+
+    Optional<Like> findByFeedIdAndUserId(UUID feedId, UUID userId);
 }
---
+++ src/main/java/com/codeit/otboo/domain/like/service/LikeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.codeit.otboo.domain.like.service;
+
+import com.codeit.otboo.domain.feed.entity.Feed;
+import com.codeit.otboo.domain.feed.repository.FeedRepository;
+import com.codeit.otboo.domain.like.entity.Like;
+import com.codeit.otboo.domain.like.repository.LikeRepository;
+import com.codeit.otboo.domain.user.entity.User;
+import com.codeit.otboo.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class LikeServiceImpl implements LikeService {
+
+    private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
+
+    @Override
+    @Transactional
+    public void feedLike(UUID feedId, UUID userId) {
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new IllegalArgumentException("feedId is invalid"));
+
+        User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new IllegalArgumentException("userId is invalid"));
+
+        if (likeRepository.existsByFeedIdAndUserId(feedId, userId))
+            throw new IllegalArgumentException("already liked");
+
+        feed.addLike(user);
+    }
+
+    @Override
+    @Transactional
+    public void feedUnlike(UUID feedId, UUID userId) {
+        Like like = likeRepository.findByFeedIdAndUserId(feedId, userId)
+                .orElseThrow(() -> new IllegalArgumentException("like is invalid"));
+
+        Feed feed = like.getFeed();
+        feed.removeLike(like);
+    }
+}
