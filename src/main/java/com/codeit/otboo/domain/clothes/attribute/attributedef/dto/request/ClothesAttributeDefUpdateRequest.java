c927654e233e09d4ed46c5c732a3616915e881b3
yumi Kim <cdcd0214@gmail.com>
2026-03-19 13:14:44 +0900

    Refactor: DTO에 @Builder 추가

--- src/main/java/com/codeit/otboo/domain/clothes/attribute/attributedef/dto/request/ClothesAttributeDefUpdateRequest.java
+++ src/main/java/com/codeit/otboo/domain/clothes/attribute/attributedef/dto/request/ClothesAttributeDefUpdateRequest.java
@@ -1,7 +1,10 @@
 package com.codeit.otboo.domain.clothes.attribute.attributedef.dto.request;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record ClothesAttributeDefUpdateRequest(
         String name,
         List<String> selectableValues
--- src/main/java/com/codeit/otboo/domain/clothes/attribute/attributevalue/dto/request/ClothesAttributeRequest.java
+++ src/main/java/com/codeit/otboo/domain/clothes/attribute/attributevalue/dto/request/ClothesAttributeRequest.java
@@ -1,7 +1,10 @@
 package com.codeit.otboo.domain.clothes.attribute.attributevalue.dto.request;
 
+import lombok.Builder;
+
 import java.util.UUID;
 
+@Builder
 public record ClothesAttributeRequest(
     UUID definitionId,
     String value
