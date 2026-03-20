adcad1b09faff27042aa03d3425fab7973c3e549
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-03-21 03:02:32 +0900

    feat: Jwt 구현, Jwt Authentication Filter 구현,  (#38)
    * feat: JWT 라이브러리 의존성 추가
    
    변경 내용
    - nimbus jwt 라이브러리 추가
    
    * feat: JWT Properties 추가
    
    변경 내용
    - PropertiesConfig로 외부에서 jwt 키 관리
    
    * feat: yml 환경 변수 세팅
    
    변경 내용
    
    * feat: Jwt Provider 및 Jwt Filter 추가
    
    * feat: 로그인에 access 토큰 및 리프레쉬 토큰 추가, Refresh 토큰 쿠키 등록
    
    * fix: jwt Registry 작업 분리
    
    * remove: 다른 패키지에 있던 JwtResponse 삭제
    
    * feat: JWT 라이브러리 의존성 추가
    
    변경 내용
    - nimbus jwt 라이브러리 추가
    
    # Conflicts:
    #	build.gradle
    
    * feat: JWT Properties 추가
    
    변경 내용
    - PropertiesConfig로 외부에서 jwt 키 관리
    
    * feat: yml 환경 변수 세팅
    
    변경 내용
    
    * feat: Jwt Provider 및 Jwt Filter 추가
    
    * feat: 로그인에 access 토큰 및 리프레쉬 토큰 추가, Refresh 토큰 쿠키 등록
    
    * fix: jwt Registry 작업 분리
    
    * remove: 다른 패키지에 있던 JwtResponse 삭제
    
    * feat: JWT 라이브러리 의존성 추가
    
    변경 내용
    - nimbus jwt 라이브러리 추가
    
    # Conflicts:
    #	build.gradle
    
    # Conflicts:
    #	build.gradle
    
    * feat: JWT Properties 추가
    
    변경 내용
    - PropertiesConfig로 외부에서 jwt 키 관리
    
    * feat: yml 환경 변수 세팅
    
    변경 내용
    
    * feat: Jwt Provider 및 Jwt Filter 추가
    
    * feat: 로그인에 access 토큰 및 리프레쉬 토큰 추가, Refresh 토큰 쿠키 등록
    
    * fix: jwt Registry 작업 분리
    
    * remove: 다른 패키지에 있던 JwtResponse 삭제
    
    * feat: JWT 라이브러리 의존성 추가
    
    변경 내용
    - nimbus jwt 라이브러리 추가
    
    * feat: JWT Properties 추가
    
    변경 내용
    - PropertiesConfig로 외부에서 jwt 키 관리
    
    * feat: yml 환경 변수 세팅
    
    변경 내용
    
    * feat: 로그인에 access 토큰 및 리프레쉬 토큰 추가, Refresh 토큰 쿠키 등록
    
    * fix: jwt Registry 작업 분리
    
    * refactor: DirectMessageControllerTest에서 JwtAuthenticationFilter 제외 설정 추가
    
    변경 내용
    - DirectMessageControllerTest에 Security 테스트 관련 excludeFilter 설정 추가
    - JwtAuthenticationFilter로 인해 발생하는 테스트 실패 방지
    
    변경 이유
    - JwtAuthenticationFilter로 인해 불필요한 테스트 장애 발생 방지 목적
    
    * feat: 테스트 환경에 JWT 설정 추가
    
    변경 내용
    - application-test.yml에 JWT 관련 설정 추가
      - secret, access-token-expiration, refresh-token-expiration, issuer 필드 포함
    
    변경 이유
    - 테스트 환경에서 JWT 토큰 동작 검증을 위한 설정 필요
    
    * fix: 반복된 환경변수 코드 삭제
    
    변경 내용
    - 병합 과정 중 jwt 환경 세팅 삭제

--- src/main/java/com/codeit/otboo/domain/user/controller/AuthController.java
+++ src/main/java/com/codeit/otboo/domain/user/controller/AuthController.java
@@ -1,23 +1,44 @@
 package com.codeit.otboo.domain.user.controller;
 
 import com.codeit.otboo.domain.user.service.AuthService;
-import com.codeit.otboo.global.security.dto.JwtResponse;
+import com.codeit.otboo.global.security.jwt.JwtProperties;
+import com.codeit.otboo.global.security.jwt.RefreshCookieFactory;
+import com.codeit.otboo.global.security.jwt.dto.JwtInformation;
+import com.codeit.otboo.global.security.jwt.dto.JwtResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final RefreshCookieFactory refreshCookieFactory;
+    private final JwtProperties jwtProperties;
 
     @PostMapping(value = "/sign-in", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<JwtResponse> signIn(@RequestPart("username") String username,
-                                              @RequestPart("password") String password) {
-        JwtResponse jwtResponse = authService.signIn(username, password);
+                                              @RequestPart("password") String password,
+                                              HttpServletResponse response) {
+        JwtInformation jwtInformation = authService.signIn(username, password);
+        long refreshTokenExpirationSeconds =
+                TimeUnit.MILLISECONDS.toSeconds(
+                        jwtProperties.refreshTokenExpiration()
+                );
+
+        Cookie cookie = refreshCookieFactory.create(
+                jwtInformation.refreshToken(),
+                refreshTokenExpirationSeconds
+        );
+
+        response.addCookie(cookie);
+        JwtResponse jwtResponse = new JwtResponse(jwtInformation.userResponse(), jwtInformation.accessToken());
         return ResponseEntity.ok(jwtResponse);
     }
-
 }
