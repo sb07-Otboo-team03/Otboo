7f63472b8343ff74b4cf8ddca7e5ed748632b6ee
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-03-20 11:15:19 +0900

    remove: 다른 패키지에 있던 JwtResponse 삭제

--- src/main/java/com/codeit/otboo/global/security/dto/JwtResponse.java
+++
@@ -1,9 +0,0 @@
-package com.codeit.otboo.global.security.dto;
-
-import com.codeit.otboo.domain.user.dto.response.UserResponse;
-
-public record JwtResponse(
-        UserResponse userDto,
-        String accessToken
-) {
-}
