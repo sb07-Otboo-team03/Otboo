91d0ed582046574371883e77390aaa0109b5207c
yumi Kim <cdcd0214@gmail.com>
2026-03-25 09:10:33 +0900

    feat : ClothesAttribute의 조회 생성(customRepository, 조회용 DTO, 조회 Test 추가)

---
+++ src/main/java/com/codeit/otboo/domain/clothes/attribute/attributedef/repository/ClothesAttributeDefCustomRepositoryImpl.java
@@ -0,0 +1,62 @@
+package com.codeit.otboo.domain.clothes.attribute.attributedef.repository;
+
+import com.codeit.otboo.domain.clothes.attribute.attributedef.dto.request.ClothesAttributeSearchRequest;
+import com.codeit.otboo.domain.clothes.attribute.attributedef.entity.ClothesAttributeDef;
+import com.codeit.otboo.domain.clothes.attribute.attributedef.entity.QClothesAttributeDef;
+import com.codeit.otboo.domain.clothes.attribute.attributevalue.entity.QClothesAttributeValue;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.ComparableExpressionBase;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static com.codeit.otboo.domain.clothes.attribute.attributedef.entity.QClothesAttributeDef.clothesAttributeDef;
+import static com.codeit.otboo.domain.clothes.attribute.attributevalue.entity.QClothesAttributeValue.clothesAttributeValue;
+
+@RequiredArgsConstructor
+public class ClothesAttributeDefCustomRepositoryImpl implements  ClothesAttributeDefCustomRepository {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<ClothesAttributeDef> searchAttributes(
+            ClothesAttributeSearchRequest clothesAttributeSearchRequest) {
+        return queryFactory.select(clothesAttributeDef)
+                .from(clothesAttributeDef)
+                .leftJoin(clothesAttributeValue)
+                .on(clothesAttributeValue.attributeDef.eq(clothesAttributeDef))
+                .where(
+                        keywordLike(clothesAttributeSearchRequest.keywordLike())
+                )
+                .distinct()
+                .orderBy(getSortOrder(
+                        clothesAttributeSearchRequest.sortBy(),
+                        clothesAttributeSearchRequest.sortDirection()))
+                .fetch();
+    }
+
+    private BooleanExpression keywordLike(String keyword) {
+        if(!StringUtils.hasText(keyword)){
+            return null;
+        }
+        return clothesAttributeDef.name.containsIgnoreCase(keyword)
+                .or(clothesAttributeValue.selectableValue.containsIgnoreCase(keyword));
+    }
+
+    private OrderSpecifier<?> getSortOrder(String sortBy, String sortDirection) {
+        Order order = (sortDirection != null && sortDirection.toUpperCase().startsWith("ASC"))
+                ? Order.ASC : Order.DESC;
+
+        ComparableExpressionBase<?> field;
+        if("name".equalsIgnoreCase(sortBy)){
+            field = clothesAttributeDef.name;
+        } else {
+            field = clothesAttributeDef.createdAt;
+        }
+
+        return new OrderSpecifier<>(order, field);
+    }
+}
