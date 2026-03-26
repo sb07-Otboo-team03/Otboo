4cdac7f39f272406317978ac7c48aa5f04569f28
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-03-26 17:43:28 +0900

    refactor: 필드, 어노테이션 email로 수정

--- src/main/java/com/codeit/otboo/domain/user/dto/request/SignInRequest.java
+++ src/main/java/com/codeit/otboo/domain/user/dto/request/SignInRequest.java
@@ -1,10 +1,11 @@
 package com.codeit.otboo.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record SignInRequest(
-        @NotBlank
+        @Email
         String username,
 
         @NotBlank
