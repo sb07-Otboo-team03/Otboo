ff80d6ce77db1f93c6beb0b44200f63383094f9e
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-03-28 21:12:11 +0900

    test: 유저 컨트롤러 테스트 회원가입 코드 작성

--- src/test/java/com/codeit/otboo/domain/user/fixture/UserCreateRequestFixture.java
+++ src/test/java/com/codeit/otboo/domain/user/fixture/UserCreateRequestFixture.java
@@ -6,4 +6,8 @@ public class UserCreateRequestFixture {
     public static UserCreateRequest create() {
         return new UserCreateRequest("test", "test12345@codeit.com", "password12345");
     }
+
+    public static UserCreateRequest create(String name, String email, String password) {
+        return new UserCreateRequest(name, email, password);
+    }
 }
