0fc7bf8bc09f39253f5a354a255d01e049d0d6c0
ChoiJiHye950 <principle950@daum.net>
2026-03-31 13:23:25 +0900

    refactor : 비동기 설정 클래스 분리
    - @EnableAsync 어노테이션을 별도의 AsyncConfig 클래스로 분리
    - Application 클래스에서 비동기 관련 설정 제거
    - 코드 모듈화 및 가독성 개선

--- src/main/java/com/codeit/otboo/OtbooApplication.java
+++ src/main/java/com/codeit/otboo/OtbooApplication.java
@@ -2,14 +2,11 @@ package com.codeit.otboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
+
 @SpringBootApplication
 public class OtbooApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OtbooApplication.class, args);
 	}
-
 }
