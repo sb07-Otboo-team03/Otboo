b4f17f8efd5cc883f5b69c404d148fa3f7905e5f
my05030 <my03160@gmail.com>
2026-04-02 13:54:47 +0900

    stash

--- src/main/java/com/codeit/otboo/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/otboo/domain/notification/repository/NotificationRepository.java
@@ -1,17 +1,22 @@
 package com.codeit.otboo.domain.notification.repository;
 
 import com.codeit.otboo.domain.notification.dto.NotificationDto;
+import com.codeit.otboo.domain.notification.dto.NotificationLevel;
 import com.codeit.otboo.domain.notification.entity.Notification;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    Optional<Notification> findByIdAndReceiver_Id(UUID id, UUID receiverId);
 
     @Query("""
         SELECT new com.codeit.otboo.domain.notification.dto.NotificationDto(
@@ -24,6 +29,7 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
         )
         FROM Notification n
         WHERE (
+            n.receiver.id = :receiverId AND
             CAST(:cursor AS timestamp) IS NULL
                 OR (
                     n.createdAt < :cursor
@@ -36,8 +42,24 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
         ORDER BY n.createdAt DESC, n.id DESC
     """)
     List<NotificationDto> findAll(
+        @Param("receiverId") UUID receiverId,
         @Param("cursor") LocalDateTime cursor,
         @Param("idAfter") UUID idAfter,
         Pageable pageable
     );
+
+//    @Modifying(clearAutomatically = true)
+//    @Query("""
+//        INSERT INTO Notification (title, content, level, receiver, batchId)
+//        SELECT :title, :content, :level, u, :batchId
+//        FROM User u
+//        WHERE u.locked = true
+//    """)
+//    int insertNotificationsToLockedUsers(
+//        @Param("title") String title,
+//        @Param("content") String content,
+//        @Param("level") NotificationLevel level,
+//        @Param("batchId") UUID batchId
+//    );
+
 }
