bddef9a678cc1c86b80f5fb255a7fd5f96ed6eec
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-04-07 03:22:03 +0900

    feat: 유저 목록 조회 기능 추가

--- src/main/java/com/codeit/otboo/domain/user/controller/UserController.java
+++ src/main/java/com/codeit/otboo/domain/user/controller/UserController.java
@@ -3,11 +3,14 @@ package com.codeit.otboo.domain.user.controller;
 import com.codeit.otboo.domain.profile.dto.response.ProfileResponse;
 import com.codeit.otboo.domain.user.dto.request.UpdatePasswordRequest;
 import com.codeit.otboo.domain.user.dto.request.UserCreateRequest;
+import com.codeit.otboo.domain.user.dto.request.UserSearchRequest;
 import com.codeit.otboo.domain.user.dto.response.UserResponse;
 import com.codeit.otboo.domain.user.service.UserService;
+import com.codeit.otboo.global.slice.dto.CursorResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,5 +42,12 @@ public class UserController {
     public ResponseEntity<Void> updatePassword(@PathVariable("userId") UUID userId, @Valid @RequestBody UpdatePasswordRequest updatePasswordRequest) {
         userService.updateUserPassword(userId, updatePasswordRequest);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<CursorResponse<UserResponse>> getCurrentUser(@ParameterObject @ModelAttribute UserSearchRequest request) {
+        CursorResponse<UserResponse> getUsers = userService.getAllUsers(request);
+        return ResponseEntity.ok(getUsers);
+
     }
 }
