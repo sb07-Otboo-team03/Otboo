a27d4b8b3bc572d6c7a1d51f096db6da82136a5d
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-04-06 16:18:52 +0900

    refactor: MailService 클래스명 수정

--- src/main/java/com/codeit/otboo/domain/user/service/MailServiceImpl.java
+++ src/main/java/com/codeit/otboo/domain/user/service/MailServiceImpl.java
@@ -4,14 +4,13 @@ import com.codeit.otboo.domain.user.exception.TemporaryPasswordMailSendFailedExc
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class SmtpMailService implements MailService{
+public class MailServiceImpl implements MailService{
 
     private final JavaMailSender mailSender;
 
--- src/test/java/com/codeit/otboo/domain/user/service/SmtpMailServiceTest.java
+++ src/test/java/com/codeit/otboo/domain/user/service/SmtpMailServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -27,7 +26,7 @@ class SmtpMailServiceTest {
     private JavaMailSender mailSender;
 
     @InjectMocks
-    private SmtpMailService smtpMailService;
+    private MailServiceImpl smtpMailService;
 
     @BeforeEach
     void setUp() {
