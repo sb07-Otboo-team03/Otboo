c0e8bd0257a03e9dbb34bf9c6b9e5e5c72001d5b
gnara0719 <ppjw254236@gmail.com>
2026-04-08 01:45:11 +0900

    Feat: Elasticsearch Feed Document 서비스 구현
    - FeedDocumentService 및 구현체 추가
    - FeedDocumentRepository 및 테스트 코드 작성
    - Elasticsearch 연동 CRUD 및 동기화 로직 구현
    - 관련 테스트 클래스 및 fixture 추가

---
+++ src/test/java/com/codeit/otboo/domain/feed/fixture/FeedDocumentFixture.java
@@ -0,0 +1,47 @@
+package com.codeit.otboo.domain.feed.fixture;
+
+import com.codeit.otboo.domain.feed.elasticsearch.document.FeedDocument;
+import com.codeit.otboo.domain.feed.entity.Feed;
+
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FeedDocumentFixture {
+
+    public static List<FeedDocument> createFeedDocument(int n) {
+
+        List<FeedDocument> docs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            docs.add(FeedDocument.builder()
+                    .id(UUID.randomUUID().toString())
+                    .content("content " + i % 2)
+                    .skyStatus(i % 2 == 0 ? "CLEAR" : "CLOUDY")
+                    .precipitationType(i % 2 == 0 ? "NONE" : "RAIN")
+                    .createdAt(100L - i)
+                    .likeCount((long) i)
+                    .build());
+        }
+
+        return docs;
+    }
+
+    public static List<FeedDocument> createFeedDocument(List<Feed> feedList, int n) {
+
+        List<FeedDocument> docs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            Long epochMilli = feedList.get(i).getCreatedAt().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+            docs.add(FeedDocument.builder()
+                    .id(feedList.get(i).getId().toString())
+                    .createdAt(epochMilli)
+                    .likeCount(feedList.get(i).getLikeCount())
+                    .build());
+        }
+
+        return docs;
+    }
+}
