c91688d40898403b7d5abfe9b0493bcce953f14a
yumi Kim <cdcd0214@gmail.com>
2026-04-08 11:22:05 +0900

    feat : RecommendationServiceImpl 추천로직2-3단계

--- src/main/java/com/codeit/otboo/domain/clothes/management/repository/ClothesRepository.java
+++ src/main/java/com/codeit/otboo/domain/clothes/management/repository/ClothesRepository.java
@@ -3,8 +3,11 @@ package com.codeit.otboo.domain.clothes.management.repository;
 import com.codeit.otboo.domain.clothes.management.entity.Clothes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ClothesRepository extends JpaRepository<Clothes, UUID>, ClothesRepositoryCustom{
     boolean existsByIdAndOwnerId(UUID id, UUID ownerId);
+
+    List<Clothes> findByOwnerId(UUID ownerId);
 }
