a6389c5aad424afef082745bff0d6eff29e080fc
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-04-09 03:37:46 +0900

    refactor: ProfileUpdateRequest 유효성 검사 수정
    변경 내용
    - 이름 필드에 공백 방지 @Pattern 어노테이션 추가
    - temperatureSensitivity 타입을 int에서 Integer로 변경
    
    변경 이유
    - 입력값 유효성을 강화하여 데이터 무결성 확보
    - temperatureSensitivity 필드의 null 허용을 위한 타입 변경

--- src/main/java/com/codeit/otboo/domain/profile/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/codeit/otboo/domain/profile/dto/request/ProfileUpdateRequest.java
@@ -2,20 +2,19 @@ package com.codeit.otboo.domain.profile.dto.request;
 
 import com.codeit.otboo.domain.profile.entity.Gender;
 import com.codeit.otboo.domain.profile.entity.Location;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 public record ProfileUpdateRequest(
+        @Pattern(regexp = "^(?!\\s*$).+", message = "공백은 허용되지 않습니다.")
         @Size(min = 2, max = 20)
         String name,
         Gender gender,
         LocalDate birthDate,
         LocationRequest location,
-        @Min(1) @Max(5)
-        int temperatureSensitivity
+        @Min(1)
+        @Max(5)
+        Integer temperatureSensitivity
 ) {
 }
