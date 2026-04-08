3f0894633e39bf644b3934587d49d7ad0f6ad479
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-04-08 21:09:38 +0900

    refactor: dto valid 추가

--- src/main/java/com/codeit/otboo/domain/profile/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/codeit/otboo/domain/profile/dto/request/ProfileUpdateRequest.java
@@ -2,14 +2,20 @@ package com.codeit.otboo.domain.profile.dto.request;
 
 import com.codeit.otboo.domain.profile.entity.Gender;
 import com.codeit.otboo.domain.profile.entity.Location;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record ProfileUpdateRequest(
+        @Size(min = 2, max = 20)
         String name,
         Gender gender,
         LocalDate birthDate,
         LocationRequest location,
+        @Min(1) @Max(5)
         int temperatureSensitivity
 ) {
 }
