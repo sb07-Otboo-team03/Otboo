6bfc49564d52476e9c0f3e65267866f025911a63
my05030 <my03160@gmail.com>
2026-04-08 21:45:34 +0900

    Feat: ClothesAttributeDefSseEvent에 userId 필드 추가 - SSE 이벤트에 필요한 사용자 식별자(userId) 필드 및 생성자 로직 추가.

--- src/main/java/com/codeit/otboo/domain/sse/event/ClothesAttributeDefSseEvent.java
+++ src/main/java/com/codeit/otboo/domain/sse/event/ClothesAttributeDefSseEvent.java
@@ -3,7 +3,10 @@ package com.codeit.otboo.domain.sse.event;
 import java.util.UUID;
 
 public class ClothesAttributeDefSseEvent extends BaseSseEvent {
+    private UUID userId;
+
     public ClothesAttributeDefSseEvent(String title, String content, UUID userId) {
         super(title, content);
+        this.userId = userId;
     }
 }
