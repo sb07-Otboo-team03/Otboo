dbec90e7b5b3ba281b8351773f400ef244a2f221
my05030 <my03160@gmail.com>
2026-04-12 03:02:11 +0900

    Refactor: KafkaProduceRequiredEventListener에 @ 위치 수정 - @Profile("prod") 어노테이션의 위치 변경으로 가독성 향상.

--- src/main/java/com/codeit/otboo/domain/kafka/KafkaProduceRequiredEventListener.java
+++ src/main/java/com/codeit/otboo/domain/kafka/KafkaProduceRequiredEventListener.java
@@ -20,9 +20,9 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Profile("prod")
 @Slf4j
 @RequiredArgsConstructor
+@Profile("prod")
 @Component
 public class KafkaProduceRequiredEventListener {
 
