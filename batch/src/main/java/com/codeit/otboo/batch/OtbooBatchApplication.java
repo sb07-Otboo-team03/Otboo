b5faed917fbcd9b8bc7b81bffe58a7524640725c
seongman Joe <smjoe0302@naver.com>
2026-04-15 22:51:08 +0900

    refactor: 필요없는 import문 제거

--- batch/src/main/java/com/codeit/otboo/batch/OtbooBatchApplication.java
+++ batch/src/main/java/com/codeit/otboo/batch/OtbooBatchApplication.java
@@ -1,13 +1,10 @@
 package com.codeit.otboo.batch;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.TimeZone;
 
 @SpringBootApplication(
         scanBasePackages = {
