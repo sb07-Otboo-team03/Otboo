90ae66efc5a02db5cbd020be415457f6736837f4
yumi Kim <cdcd0214@gmail.com>
2026-04-15 23:39:44 +0900

    feat : RecommendationService 로직 수정, AI등록 로직

---
+++ src/main/java/com/codeit/otboo/global/config/RandomConfig.java
@@ -0,0 +1,17 @@
+package com.codeit.otboo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class RandomConfig
+{
+
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+    
+}
