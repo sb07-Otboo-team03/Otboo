32fee52e5326fbd3ccc70ddf7683596f8eff6fed
ChoiJiHye950 <principle950@daum.net>
2026-04-16 10:16:15 +0900

    test : S3 스토리지 경로 프로퍼티 추가 및 테스트 수정
    - S3BinaryContentStorage에 StoragePathProperties 적용
    - @ConfigurationProperties 및 @EnableConfigurationProperties 활용해 설정 관리 개선
    - S3BinaryContentStorageTest에 스토리지 경로 프로퍼티 주입 로직으로 변경
    - 바이너리 데이터 전체 삭제 테스트 케이스 추가로 로직 검증 강화
    
    BREAKING CHANGE: 없음

--- api/src/test/java/com/codeit/otboo/domain/binarycontent/unit/storage/S3BinaryContentStorageTest.java
+++ api/src/test/java/com/codeit/otboo/domain/binarycontent/unit/storage/S3BinaryContentStorageTest.java
@@ -1,11 +1,13 @@
 package com.codeit.otboo.domain.binarycontent.unit.storage;
 
 import com.codeit.otboo.domain.binarycontent.storage.s3.S3BinaryContentStorage;
+import com.codeit.otboo.global.properties.StoragePathProperties;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.Resource;
@@ -15,20 +17,31 @@ import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class S3BinaryContentStorageTest {
+    private static final String BUCKET = "test-bucket";
+    private static final String PATH = "binary";
+
     @Mock
     private S3Client s3Client;
 
-    @InjectMocks
-    private S3BinaryContentStorage storage;
+    private S3BinaryContentStorage s3BinaryContentStorage;
+
+    @BeforeEach
+    void setUp() {
+        StoragePathProperties storagePathProperties = new StoragePathProperties(
+                new StoragePathProperties.S3(PATH));
+        s3BinaryContentStorage = new S3BinaryContentStorage(s3Client, BUCKET, storagePathProperties);
+    }
 
     @Nested
     @DisplayName("바이너리 데이터 조회")
@@ -47,7 +60,7 @@ public class S3BinaryContentStorageTest {
             given(s3Client.getObject(any(GetObjectRequest.class))).willReturn(responseStream);
 
             // when
-            InputStream inputStream = storage.get(binaryContentId);
+            InputStream inputStream = s3BinaryContentStorage.get(binaryContentId);
 
             // then
             assertThat(inputStream.readAllBytes()).isEqualTo(data);
@@ -72,7 +85,7 @@ public class S3BinaryContentStorageTest {
                     .willReturn(responseStream);
 
             // when
-            Resource resource = storage.download(binaryContentId);
+            Resource resource = s3BinaryContentStorage.download(binaryContentId);
 
             // then
             assertThat(resource).isNotNull();
@@ -93,10 +106,68 @@ public class S3BinaryContentStorageTest {
                     .willReturn(DeleteObjectResponse.builder().build());
 
             // when
-            storage.delete(binaryContentId);
+            s3BinaryContentStorage.delete(binaryContentId);
 
             // then
             then(s3Client).should().deleteObject(any(DeleteObjectRequest.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("대상 바이너리 전체 삭제")
+    class DeleteAllBinaryContent {
+        @Test
+        @DisplayName("성공: binaryIds 가 null 이면 메소드가 바로 종료된다")
+        void success_delete_all_with_null() {
+            // given
+            List<UUID> binaryIds = null;
+
+            // when
+            s3BinaryContentStorage.deleteAll(binaryIds);
+
+            // then
+            then(s3Client).should(never()).deleteObjects(any(DeleteObjectsRequest.class));
+        }
+
+        @Test
+        @DisplayName("성공: binaryIds가 빈 리스트이면 메소드가 바로 종료된다")
+        void success_delete_all_with_empty_list() {
+            // given
+            List<UUID> binaryIds = List.of();
+
+            // when
+            s3BinaryContentStorage.deleteAll(binaryIds);
+
+            // then
+            then(s3Client).should(never()).deleteObjects(any(DeleteObjectsRequest.class));
+        }
+
+        @Test
+        @DisplayName("성공: binaryIds 가 존재하면 DeleteObjectsRequest 를 만들어 S3 삭제를 요청한다")
+        void success_delete_all() {
+            // given
+            UUID binaryId1 = UUID.randomUUID();
+            UUID binaryId2 = UUID.randomUUID();
+            List<UUID> binaryIds = List.of(binaryId1, binaryId2);
+
+            // when
+            s3BinaryContentStorage.deleteAll(binaryIds);
+
+            // then
+            ArgumentCaptor<DeleteObjectsRequest> captor =
+                    ArgumentCaptor.forClass(DeleteObjectsRequest.class);
+
+            then(s3Client).should(times(1)).deleteObjects(captor.capture());
+
+            DeleteObjectsRequest deleteObjectsRequest = captor.getValue();
+            Delete deleteRequest = deleteObjectsRequest.delete();
+            List<ObjectIdentifier> targetList = deleteRequest.objects();
+
+            assertThat(deleteObjectsRequest.bucket()).isEqualTo(BUCKET);
+            assertThat(deleteRequest).isNotNull();
+            assertThat(targetList).hasSize(binaryIds.size());
+            assertThat(targetList.get(0).key()).isEqualTo(PATH + "/" + binaryId1);
+            assertThat(targetList.get(1).key()).isEqualTo(PATH + "/" + binaryId2);
         }
     }
 }
